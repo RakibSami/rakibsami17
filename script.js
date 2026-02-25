c29c46abc0f8e6ce7787fe0e185874d448f35bc9
Rakib Hasan Sami <92004808+RakibSami@users.noreply.github.com>
2026-02-25 13:41:26 +0600

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-// Hover sound setup
 const hoverSound = document.getElementById('hoverSound');
 const buttons = document.querySelectorAll('.btn');
 
+
+function unlockAudio() {
+    hoverSound.play().then(() => {
+        hoverSound.pause();
+        hoverSound.currentTime = 0;
+    }).catch(() => {});
+    
+    document.removeEventListener('click', unlockAudio);
+    document.removeEventListener('keydown', unlockAudio);
+}
+
+
+document.addEventListener('click', unlockAudio);
+document.addEventListener('keydown', unlockAudio);
+
+
 buttons.forEach(btn => {
     btn.addEventListener('mouseenter', () => {
-        hoverSound.currentTime = 0;        
-        hoverSound.play().catch(error => {
-           
-            console.log("Hover sound error:", error);
-        });
+        hoverSound.currentTime = 0;
+        hoverSound.play().catch(() => {});
     });
-
-    
 });
